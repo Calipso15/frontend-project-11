b95480700ff01a8a7cc0c443cd8b56dc56edfa50
Kris <kris@MacBook-Air-Kris.local>
2023-09-06 23:13:19 +0300

    bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,23 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
 
     // Add your rules for custom modules here
