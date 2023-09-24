19058c138da3d53cec31d2c256a1710548a896f6
Kris <kris@MacBook-Air-Kris.local>
2023-09-24 21:34:45 +0200

    refactoring2

--- src/view.js
+++ src/view.js
@@ -44,6 +44,35 @@ const handleErrors = (input, feedback, errorMessage) => {
   showError(input, feedback, errorMessage);
 };
 
+const fetchAndHandleResponse = (inputValue, input, feedback) => {
+  const corsProxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(inputValue)}`;
+
+  return fetch(corsProxyUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const htmlCode = data.contents;
+      if (!htmlCode.includes('<rss')) {
+        state.isValid = false;
+        state.errorMessage = i18n.t(yupMessages.string.notValidateUrl);
+        showError(input, feedback, state.errorMessage);
+      } else {
+        showSuccess(input, feedback, i18n.t(yupMessages.string.rssLoaded));
+        input.focus();
+        const inputVal = input;
+        inputVal.value = '';
+      }
+      if (state.isValid) {
+        state.rssFeeds.push(inputValue);
+        loadRSSFeed(inputValue);
+        document.querySelector('.posts').innerHTML = '';
+        document.querySelector('.feeds').innerHTML = '';
+      }
+    })
+    .catch(() => {
+      handleErrors(input, feedback, i18n.t(yupMessages.mixed.default));
+    });
+};
+
 const initializeView = () => {
   const input = document.getElementById('url-input');
   const feedback = document.querySelector('.feedback');
@@ -63,30 +92,8 @@ const initializeView = () => {
           showError(input, feedback, state.errorMessage);
           return;
         }
-        const corsProxyUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(inputValue)}`;
-        fetch(corsProxyUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            const htmlCode = data.contents;
-            if (!htmlCode.includes('<rss')) {
-              state.isValid = false;
-              state.errorMessage = i18n.t(yupMessages.string.notValidateUrl);
-              showError(input, feedback, state.errorMessage);
-            } else {
-              showSuccess(input, feedback, i18n.t(yupMessages.string.rssLoaded));
-              input.focus();
-              input.value = '';
-            }
-            if (state.isValid) {
-              state.rssFeeds.push(inputValue);
-              loadRSSFeed(inputValue);
-              document.querySelector('.posts').innerHTML = '';
-              document.querySelector('.feeds').innerHTML = '';
-            }
-          })
-          .catch(() => {
-            handleErrors(input, feedback, i18n.t(yupMessages.mixed.default));
-          });
+
+        fetchAndHandleResponse(inputValue, input, feedback);
       } catch (error) {
         handleErrors(input, feedback, i18n.t(yupMessages.string.notCorrectUrl));
       }
