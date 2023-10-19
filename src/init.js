c2ed76cbb7d54a2f6bc5233d0a9c8a357b4f240d
Kris <kris@MacBook-Air-Kris.local>
2023-10-19 02:09:54 +0200

    linter

--- src/init.js
+++ src/init.js
@@ -1,26 +1,24 @@
 import i18n from 'i18next';
 import yupMessages from './message';
 
-const initI18n = () => {
-  return new Promise((resolve, reject) => {
-    i18n
-      .init({
-        lng: 'ru',
-        debug: false,
-        resources: {
-          ru: {
-            translation: yupMessages,
-          },
+const initI18n = () => new Promise((resolve, reject) => {
+  i18n
+    .init({
+      lng: 'ru',
+      debug: false,
+      resources: {
+        ru: {
+          translation: yupMessages,
         },
-      })
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
-};
+      },
+    })
+    .then(() => {
+      resolve();
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
 
 initI18n();
 
